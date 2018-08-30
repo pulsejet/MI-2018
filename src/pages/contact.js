5e4153402f275c9c3515e5b5b14c034ba1b2f30b
Varun Patil <radialapps@gmail.com>
2018-08-30 20:24:18 +0530

    Dynamicall create team page

---
+++ src/pages/contact.js
@@ -0,0 +1,47 @@
+teams = [
+    {
+        name: 'COMPETITIONS &amp; LYP',
+        short_name: 'compi',
+        people: [
+            {
+                name: 'PRANIL JAIN',
+                image: 'pranil.JPG',
+                tel: '+91-9876543210',
+                email: 'pranil@moodi.org'
+            },
+            {
+                name: 'SANJAY KHATRI',
+                image: 'pranil.JPG',
+                tel: '+91-9999999999',
+                email: 'sanjay@moodi.org'
+            }
+        ]
+    },
+    {
+        name: 'CREATIVES',
+        short_name: 'creatives',
+        people: [
+            {
+                name: 'SAYAN DAS',
+                image: 'pranil.JPG',
+                tel: '+91-9876543210',
+                email: 'sayan@moodi.org',
+                department: 'DESIGN'
+            },
+            {
+                name: 'ARSH AKTHAR ANSARI',
+                image: 'pranil.JPG',
+                tel: '+91-9999999999',
+                email: 'arsh@moodi.org',
+                department: 'WEB'
+            },
+            {
+                name: 'SUDHANSHU VERMA',
+                image: 'pranil.JPG',
+                tel: '+91-9999999999',
+                email: 'sudhanshu@moodi.org',
+                department: 'AMBIENCE'
+            }
+        ]
+    }
+];
