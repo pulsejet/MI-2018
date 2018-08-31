cbaaf52565f2f8f9f6483a7bee0012f89be28a98
Varun Patil <radialapps@gmail.com>
2018-08-31 20:27:33 +0530

    Prettify particles.js config

--- src/script/particle_config.js
+++ src/script/particle_config.js
@@ -1,127 +1,114 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+import 'particles.js';
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-
-/* Otherwise just put the config content (json): */
-
-particlesJS('particles-js',
-
-{
-  "particles": {
-    "number": {
-      "value": 160,
-      "density": {
-        "enable": false,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#000000"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 1,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 1,
-        "opacity_min": 0,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 4,
-        "size_min": 0.3,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": !(window.innerWidth <= 800 && window.innerHeight <= 600),
-      "speed": 1,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 600
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "bubble"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
+export default function() {
+  particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 160,
+            density: {
+                enable: !1,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#000000"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
+            },
+            polygon: {
+                nb_sides: 5
+            },
+            image: {
+                src: "",
+                width: 100,
+                height: 100
+            }
+        },
+        opacity: {
+            value: 1,
+            random: !0,
+            anim: {
+                enable: !0,
+                speed: 1,
+                opacity_min: 0,
+                sync: !1
+            }
+        },
+        size: {
+            value: 3,
+            random: !0,
+            anim: {
+                enable: !1,
+                speed: 4,
+                size_min: .3,
+                sync: !1
+            }
+        },
+        line_linked: {
+            enable: !1,
+            distance: 150,
+            color: "#ffffff",
+            opacity: .4,
+            width: 1
+        },
+        move: {
+            enable: !(window.innerWidth <= 800 && window.innerHeight <= 600),
+            speed: 1,
+            direction: "none",
+            random: !0,
+            straight: !1,
+            out_mode: "out",
+            bounce: !1,
+            attract: {
+                enable: !1,
+                rotateX: 600,
+                rotateY: 600
+            }
         }
-      },
-      "bubble": {
-        "distance": 250,
-        "size": 0,
-        "duration": 2,
-        "opacity": 0,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 400,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": false
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: !1,
+                mode: "bubble"
+            },
+            onclick: {
+                enable: !0,
+                mode: "push"
+            },
+            resize: !0
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 250,
+                size: 0,
+                duration: 2,
+                opacity: 0,
+                speed: 3
+            },
+            repulse: {
+                distance: 400,
+                duration: .4
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
+    },
+    retina_detect: !1
+  });
 }
-
-);
