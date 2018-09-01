f890e66f0cb949b690dcdb3c20cfa3d3d433d0df
Varun Patil <radialapps@gmail.com>
2018-09-01 05:42:10 +0530

    Complete team page

--- src/script/people.js
+++ src/script/people.js
@@ -5,14 +5,14 @@ var teams = [
         people: [
             {
                 name: 'PRANIL JAIN',
-                image: 'pranil.JPG',
-                tel: '+91-9876543210',
+                image: 'pranil.jpg',
+                tel: '+91-8291473726',
                 email: 'pranil@moodi.org'
             },
             {
                 name: 'SANJAY KHATRI',
-                image: 'pranil.JPG',
-                tel: '+91-9999999999',
+                image: 'sanjay.jpg',
+                tel: '+91-9782600920',
                 email: 'sanjay@moodi.org'
             }
         ]
@@ -23,24 +23,193 @@ var teams = [
         people: [
             {
                 name: 'SAYAN DAS',
-                image: 'pranil.JPG',
-                tel: '+91-9876543210',
+                image: 'sayan.jpg',
+                tel: '+91-9987092577',
                 email: 'sayan@moodi.org',
                 department: 'DESIGN'
             },
             {
                 name: 'ARSH AKTHAR ANSARI',
-                image: 'pranil.JPG',
-                tel: '+91-9999999999',
+                image: 'arsh.jpg',
+                tel: '+91-9819751710',
                 email: 'arsh@moodi.org',
                 department: 'WEB'
             },
             {
                 name: 'SUDHANSHU VERMA',
-                image: 'pranil.JPG',
-                tel: '+91-9999999999',
+                image: 'sudhanshu.jpg',
+                tel: '+91-7568289305',
                 email: 'sudhanshu@moodi.org',
                 department: 'AMBIENCE'
+            }
+        ]
+    },
+    {
+        name: 'FOOD &amp; BEVERAGES',
+        short_name: 'FnB',
+        people: [
+            {
+                name: 'KABIR TANDON',
+                image: 'kabir.jpg',
+                tel: '+91-8291472037',
+                email: 'kabir@moodi.org',
+
+            }
+        ]
+    },
+    {
+        name: 'HORIZONS &amp; WORKSHOPS',
+        short_name: 'horizons',
+        people: [
+            {
+                name: 'ADITI MAHAJAN',
+                image: 'aditi.jpg',
+                tel: '+91-8291689848',
+                email: 'aditi@moodi.org'
+            },
+            {
+                name: 'ANIMESH SINGHAL',
+                image: 'animesh.jpg',
+                tel: '+91-8692803803',
+                email: 'animesh@moodi.org'
+            },
+	        {
+                name: 'HRITWIK RANJAN',
+                image: 'hritwik.jpg',
+                tel: '+91-7503771323',
+                email: 'hritwik@moodi.org',
+            }
+        ]
+    },
+    {
+        name: 'HOSPITALITY, ASSISTANCE &amp; PR',
+        short_name: 'hospi',
+        people: [
+            {
+                name: 'HIMANK JAIN',
+                image: 'himank.jpg',
+	            tel: '+91-7073543859',
+                email: 'himank@moodi.org'
+            },
+            {
+                name: 'MRIGANK MAHAJAN',
+                image: 'mrigank.jpg',
+                tel: '+91-8169223068',
+                email: 'mrigank@moodi.org'
+            }
+        ]
+    },
+    {
+        name: 'INFORMALS, STREETS &amp; LOUNGES',
+        short_name: 'informals',
+        people: [
+            {
+                name: 'AKSHAT PARWAL',
+                image: 'akshat.jpg',
+	            tel: '+91-8169918299',
+                email: 'akshat@moodi.org'
+            },
+            {
+                name: 'SHAURYA SINDHU',
+                image: 'shaurya.jpg',
+                tel: '+91-9216708270',
+                email: 'shaurya@moodi.org'
+            }
+        ]
+    },
+    {
+        name: 'MARKETING',
+        short_name: 'marki',
+        people: [
+            {
+                name: 'PRASHAM AGRAWAL',
+                image: 'prasham.jpg',
+                tel: '+91-8120497855',
+                email: 'prasham@moodi.org'
+            },
+            {
+                name: 'ABHINAV DHAKRAS',
+                image: 'abhinav.jpg',
+                tel: '+91-9619977724',
+                email: 'abhinav@moodi.org'
+            },
+	        {
+                name: 'MITESH MUNDRA',
+                image: 'mitesh.jpg',
+                tel: '+91-8291473801',
+                email: 'mitesh@moodi.org',
+            }
+        ]
+    },
+    {
+        name: 'MEDIA &amp; PUBLICITY',
+        short_name: 'informals',
+        people: [
+            {
+                name: 'KRITI KAMNA',
+                image: 'kriti.jpg',
+	            tel: '+91-7783024891',
+                email: 'kriti@moodi.org'
+            },
+            {
+                name: 'TANMAY JAIN',
+                image: 'tanmay.jpg',
+                tel: '+91-9462901544',
+                email: 'tanmay@moodi.org'
+            }
+        ]
+    },
+    {
+        name: 'PRONITES',
+        short_name: 'pronites',
+        people: [
+            {
+                name: 'VINEET SRIVASTAVA',
+                image: 'vineet.jpg',
+                tel: '+91-9003803473',
+                email: 'vineet@moodi.org'
+            },
+	        {
+                name: 'VEDANT LADHA',
+                image: 'vedant.jpg',
+                tel: '+91-9167843065',
+                email: 'vedant@moodi.org',
+            }
+        ]
+    },
+    {
+        name: 'SERVICES',
+        short_name: 'services',
+        people: [
+            {
+                name: 'GOVIND MENON',
+                image: 'govind.jpg',
+                tel: '+91-8149797946',
+                email: 'govind@moodi.org'
+            },
+	        {
+                name: 'RUSHABH REDASANI',
+                image: 'rushabh.jpg',
+                tel: '+91-8983097108',
+                email: 'rushabh@moodi.org',
+            }
+        ]
+    },
+    {
+        name: 'OVERALL COORDINATORS',
+        short_name: 'OCs',
+        people: [
+            {
+                name: 'NIKHIL VAIDYA',
+                image: 'nikhil.jpg',
+                tel: '+91-9004932980',
+                email: 'nikhil@moodi.org'
+            },
+	        {
+                name: 'SANKET AGRAWAL',
+                image: 'sanket.jpg',
+                tel: '+91-7045800191',
+                email: 'sanket@moodi.org',
             }
         ]
     }
